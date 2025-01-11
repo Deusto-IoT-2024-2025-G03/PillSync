d292f2b1d5f13a5a88e77f1b6dceba1ded1373fb
arrieniker <iker.arrien@opendeusto.es>
2025-01-11 13:05:15 +0100

    lcd.py finished

--- apps/raspi/lcd.py
+++ apps/raspi/lcd.py
@@ -19,27 +19,34 @@ class Lcd:
 	def move_message(self):
 		for sender, message in self.messages.items():
 			length = len(message) - 15  # Longitud del mensaje que cabe en la pantalla
-
+			GPIO.output(self.button - 1, True)
 			if length > 0:
-				while True:
-					GPIO.output(self.button - 1, True)
-					for i in range(length + 1):
-						if not GPIO.input(self.button):
-							# Si el botón está presionado
-							GPIO.output(self.button - 1, False)
-							print("Botón presionado, deteniendo el movimiento.")
-							break
-						else:
-							self.lcd.clear()
-							self.lcd.setCursor(1, 0)
-							self.lcd.write(sender)
-							self.lcd.setCursor(0, 0)
-							self.lcd.write(message[i:i + 16])  # Mostrar porciones del mensaje
-							time.sleep(0.4)
+				for i in range(length + 1):
+					if not GPIO.input(self.button):
+						# Si el botón está presionado
+						GPIO.output(self.button - 1, False)
+						print("Boton presionado, siguiente mensaje")
+						time.sleep(1)
+						break
+					else:
+						self.lcd.clear()
+						self.lcd.setCursor(0, 0)
+						self.lcd.write(message[i:i + 16])  # Mostrar porciones del mensaje
+						self.lcd.setCursor(1, 0)
+						self.lcd.write(sender)
+						time.sleep(0.5)
 
 			else:
+				self.lcd.clear()
+				self.lcd.setCursor(0, 0)
 				self.lcd.write(message)  # Si el mensaje cabe, mostrarlo sin movimiento
-				time.sleep(2)
+				self.lcd.setCursor(1, 0)
+				self.lcd.write(sender)
+				while True:
+					if not GPIO.input(self.button):
+						GPIO.output(self.button -1, False)
+						print("Boton presionado, siguiente mensaje")
+						break
 
 
 # Suponiendo que ya tienes una estructura como esta
@@ -47,11 +54,12 @@ if __name__ == "__main__":
 	messages = {
 		"Sender1": "Este es un mensaje largo que se moverá.",
 		"Sender2": "Otro mensaje para mover en la pantalla LCD.",
+		"Sender3": "mensaje"
 	}
 
 	button_pin = 23  # Número de pin donde tienes conectado el botón
 	GPIO.setmode(GPIO.BCM)
 	GPIO.setup(22, GPIO.OUT)
-	GPIO.setup(23, GPIO.IN, pull_up_down=GPIO_PUD_UP)
-	lcd = Lcd(messages, button_pin)
+	GPIO.setup(23, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+	lcd = Lcd(messages)
 	lcd.move_message()
